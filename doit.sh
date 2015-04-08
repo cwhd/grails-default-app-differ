fe283526a5663ce00dea4b12ac6348bd84bb55f5
cdav18 <chris.davis@nike.com>
2015-04-08 13:20:55 -0700

    create-app from $previous to $i

--- doit.sh
+++ doit.sh
@@ -5,15 +5,17 @@
 grails_versions=("2.4.3" "2.4.4" "2.4.5" "2.5.0" "3.0.0" "3.0.1")
 source ~/.gvm/bin/gvm-init.sh
 git init
+previous="n/a"
 for i in "${grails_versions[@]}"
-do 
-	echo $i
+do 	
+    echo 'create-app from $previous to $i'
 	echo -e "n" | gvm install grails $i
 	gvm use grails $i 
 	grails create-app gupdatediff
     git add --all
-    git commit -m 'grails create-app with [version]'
+    git commit -m 'create-app from $previous to $i'
     cd gupdatediff
     rm -rf *
     cd ..
+    previous=$i
 done
